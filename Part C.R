bcd457d87e01007594478d3a310cfe496aa3cb48
mkalluri19 <mkalluri@uoguelph.ca>
2018-11-20 11:12:19 -0500

    final commits

--- Part C.R
+++ Part C.R
@@ -1,4 +1,11 @@
 #Going to be investigating whether closely related species live in the smae or geographically close locations, or whether species are spread randomly geographically regardlss of relateness. As a secondary question I'm going to look at what proportion of Daphnia species were sampled from Canada.
+
+#install necessary packages
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("readr")
+library(readr)
+
 #Download the Daphnia data file from the BOLD database
 DaphniaC <- read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?taxon=Daphnia&format=tsv")
 
@@ -39,6 +46,18 @@ DaphniaC_df2 <- DaphniaC_df[complete.cases(DaphniaC_df), ]
 View(DaphniaC_df2)
 
 #compare accession numbers from the df generated from BOLD with the list of species from part B once the sequences were clustered into OTU. looking for overlap in accession numbers in order to look at which species present i can make a phylogeny for in order to look at relatedness between species
+
+#install necessary packages
+source("http://bioconductor.org/biocLite.R")
+biocLite("Biostrings")
+library(Biostrings)
+biocLite("muscle")
+library(muscle)
+biocLite("DECIPHER")
+library(DECIPHER)
+biocLite("ape")
+library(ape)
+
 #first have to run alignment on the data with muscle 
 #preform a multiple sequence alignment
 #first convert df to DNAstringset
@@ -93,12 +112,15 @@ install.packages("rgdal")
 install.packages("dismo")
 install.packages("XML")
 install.packages("maps")
-
+install.packages("maptools")
+install.packages("ggplot2")
 library(raster)
 library(rgdal)
 library(dismo)
 library(XML)
 library(maps)
+library(maptools)
+library(ggplot2)
 
 #show world map
 map()
@@ -106,14 +128,9 @@ map()
 #using lat and lon locate where individuals are found on the map
 DaphniaC_map <- points(DaphniaC_df2$lon, DaphniaC_df2$lat, col = "red", pch = 19, xlab = "Longitude", ylab = "Latitude")
 
-#add labels
-map(database = "Canada")
-DaphniaC_map2 <- points(text(x = DaphniaC_df2$lon, y = DaphniaC_df2$lat, DaphniaC_df2$species_name, pos = 4, col = "red", pch = 20))
-DaphniaC_map2
 
 #zoom in
 DaphniaC_map_canada <- plot(DaphniaC_df2$lon, DaphniaC_df2$lat, xlim = c(-160, -25), ylim = c(20, 90), col = "red", pch = 19, xlab = "Longitude", ylab = "Latitude")
-
 map(add = T)
 
 #try again to create base world map
@@ -125,49 +142,13 @@ mp <- ggplot() + mapworld
 mp <- mp + geom_point(aes(x = DaphniaC_df2$lon, y = DaphniaC_df2$lat), color = "blue", size = 3)
 mp
 
-#add labels 
-mp2 <- mp
-mp2 <- mp + geom_point() + geom_label(label = rownames(DaphniaC_df2$species_name), nudge_x = 0.25, nudge_y = 0.25)
-mp2
-
-#so that doesn't add species names so lets try again 
-#try again using standard map packages
-install.packages(c("maps", "mapdata"))
-
 #load the ggmap package
 devtools::install_github("dkahle/ggmap")
 
 #load libraries 
-library(tidyverse)
-library(mapdata)
-library(maps)
 library(stringr)
-
 install.packages("viridis")
 library(viridis)
-
-#turn map data into data frame
-help(package = "maps")
-world <- map_data("world")
-
-#look to make sure conversion worked
-head(world)
-
-#draw a simple map
-global <- map_data("world")
-ggplot() + geom_polygon(data = global, aes(x = long, y = lat, group = group)) +
-  coord_fixed(1.3)
-
-#add colour
-ggplot() +
-  geom_polygon(data = global, aes(x = long, y = lat, group = group), fill = NA, color = "red") + 
-  coord_fixed(1.3)
-
-#add colour and fill
-gg1 <- ggplot() +
-  geom_polygon(data = global, aes(x = long, y = lat, group = group), fill = "green", color = "blue") +
-  coord_fixed(1.3)
-gg1
 
 #add data points
 gg1 +
